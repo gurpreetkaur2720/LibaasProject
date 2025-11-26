483c728b2672fb419adefcf73bd105bbce842356
prasanjitagt <study.prasenjit@gmail.com>
2025-11-26 10:41:33 +0530

    made adjusments in frontend pics

--- libaas/src/components/Home.jsx
+++ libaas/src/components/Home.jsx
@@ -6,26 +6,26 @@ import bannerImg from "../assets/images/bb6.jpg";
 
 // ⭐ Updated Featured Products (MongoDB _id)
 const featuredProducts = [
-  { _id: "691b51c1a4692182a0cd6098", image: require("../assets/images/bb1.jpg"), name: "Noor-e-Nazaakat", price: 1999 },
-  { _id: "691b51c1a4692182a0cd6099", image: require("../assets/images/bb2.jpg"), name: "Zehra", price: 1799 },
-  { _id: "691b51c1a4692182a0cd609a", image: require("../assets/images/bb3.jpg"), name: "Sitara", price: 1899 },
-  { _id: "691b51c1a4692182a0cd609b", image: require("../assets/images/bb4.jpg"), name: "Mehekti Shaam", price: 1699 },
-  { _id: "691b51c1a4692182a0cd609c", image: require("../assets/images/bb5.jpg"), name: "Chandni Raat", price: 2099 },
-  { _id: "691b51c1a4692182a0cd609d", image: require("../assets/images/bb6.jpg"), name: "Husn-e-Haya", price: 1999 },
-  { _id: "691b51c1a4692182a0cd609e", image: require("../assets/images/bb7.jpg"), name: "Gulzar-e-Ishq", price: 1899 },
-  { _id: "691b51c1a4692182a0cd60ac", image: require("../assets/images/pehrin10.jpg"), name: "Sahiba", price: 1599 },
+  { _id: "691b51c1a4692182a0cd6098", image: "/images/bb1.jpg", name: "Noor-e-Nazaakat", price: 1999 },
+  { _id: "691b51c1a4692182a0cd6099", image: "/images/bb2.jpg", name: "Zehra", price: 1799 },
+  { _id: "691b51c1a4692182a0cd609a", image: "/images/bb3.jpg", name: "Sitara", price: 1899 },
+  { _id: "691b51c1a4692182a0cd609b", image: "/images/bb4.jpg", name: "Mehekti Shaam", price: 1699 },
+  { _id: "691b51c1a4692182a0cd609c", image: "/images/bb5.jpg", name: "Chandni Raat", price: 2099 },
+  { _id: "691b51c1a4692182a0cd609d", image: "/images/bb6.jpg", name: "Husn-e-Haya", price: 1999 },
+  { _id: "691b51c1a4692182a0cd609e", image: "/images/bb7.jpg", name: "Gulzar-e-Ishq", price: 1899 },
+  { _id: "691b51c1a4692182a0cd60ac", image: "/images/pehrin10.jpg", name: "Sahiba", price: 1599 },
 ];
 
 // ⭐ Updated Gallery
 const galleryImages = [
-  require("../assets/images/pehrin1.jpg"),
-  require("../assets/images/pehrin2.jpg"),
-  require("../assets/images/pehrin3.jpg"),
-  require("../assets/images/pehrin4.jpg"),
-  require("../assets/images/pehrin5.jpg"),
-  require("../assets/images/pehrin6.jpg"),
-  require("../assets/images/pehrin7.jpg"),
-  require("../assets/images/pehrin9.jpg"),
+  "/images/pehrin1.jpg",
+  "/images/pehrin2.jpg",
+  "/images/pehrin3.jpg",
+  "/images/pehrin4.jpg",
+  "/images/pehrin5.jpg",
+  "/images/pehrin6.jpg",
+  "/images/pehrin7.jpg",
+  "/images/pehrin9.jpg",
 ];
 
 export default function Home() {
